1bc49a10cf7beb8a4cbe45722952dff885332b82
Lutho362 <lutho470@gmail.com>
2023-11-22 15:12:33 +0200

    made changes and added footer

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 var accordion = document.getElementById('accordion');
 var trans = document.getElementById('trans')
 var accord_class  = accordion.classList
+var nav = document.getElementById('nav'); 
+var lastScrollY = window.scrollY; 
+
+
+window.addEventListener('scroll', ()=>{
+    if (lastScrollY < window.scrollY){
+        // console.log(lastScrollY)
+        nav.classList.add('hide-nav');
+    }else{
+        nav.classList.remove('hide-nav');
+    }
+    lastScrollY = window.scrollY;
+})
 
 trans.addEventListener('click', ()=>{
     accord_class.toggle('accordion') ;   
 })
+
+
