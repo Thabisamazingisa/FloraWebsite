dc57fd745dde9455f4e9a647ef60614a420801ea
Samkelwe Wakeni <samkelwe@freshmarksystems.co.za>
2023-11-25 02:57:51 +0200

    Updated index, html style and index js

--- index.js
+++ index.js
@@ -4,16 +4,60 @@ var accord_class  = accordion.classList
 var nav = document.getElementById('nav'); 
 var lastScrollY = window.scrollY; 
 
-
-window.addEventListener('scroll', ()=>{
-    if (lastScrollY < window.scrollY){
-        // console.log(lastScrollY)
-        nav.classList.add('hide-nav');
-    }else{
-        nav.classList.remove('hide-nav');
-    }
-    lastScrollY = window.scrollY;
+$(window).ready(function (e) {
+    let scroll_pos = $(window).scrollTop();
+    scroller();
 })
+
+$('#to-top').addClass('hide').removeClass('show');
+
+$(window).on("scroll", function () {
+    scroller()
+});
+
+function scroller() {
+    if ($(window).scrollTop() > 100) {
+        $('#nav').removeClass('hide-nav').addClass('show-nav').css('background', '#ffff');
+        $('#to-top').addClass('show').removeClass('hide');
+    } else {
+        $('#nav').removeClass('hide-nav').addClass('show-nav').css('background', 'transparent');
+        $('#to-top').addClass('hide').removeClass('show');
+    }
+}
+
+
+$('#to-top').on('click', function () {
+    smoothScroll()
+
+});
+
+$(".nav-link").on('click', function (e) {
+    e.preventDefault();
+
+    var hash = this.hash;
+
+    $('html, body').animate({
+        scrollTop: $(hash).offset().top
+    }, 1000, function () {
+        window.location.hash = hash;
+    });
+});
+
+function smoothScroll() {
+    $('html, body').animate({
+        scrollTop: $('html, body').offset().top,
+    }, 800);
+    }
+
+// window.addEventListener('scroll', ()=>{
+//     if (lastScrollY < window.scrollY){
+//         // console.log(lastScrollY)
+//         nav.classList.add('hide-nav');
+//     }else{
+//         nav.classList.remove('hide-nav');
+//     }
+//     lastScrollY = window.scrollY;
+// })
 
 trans.addEventListener('click', ()=>{
     accord_class.toggle('accordion') ;   
